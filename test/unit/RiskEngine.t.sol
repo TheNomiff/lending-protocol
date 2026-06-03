3f6067a2fc8fc041604febf7a295f55262135a56
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-03 08:36:56 +0000

    test: add RiskEngine withdraw validation coverage

--- test/unit/RiskEngine.t.sol
+++ test/unit/RiskEngine.t.sol
@@ -130,9 +130,32 @@ contract RiskEngineTest is RiskTestBase {
     ////// WITHDRAW TESTS ///////////
     //////////////////////////////////
 
-    function testCanWithdrawReturnsTrue() public {}
+    function testCanWithdrawReturnsTrue() public view {
+        uint256 remainingCollateralUsd = 90 ether;
+        uint256 debtUsd = 40 ether;
 
-    function testCanWithdrawReturnsFalse() public {}
+        bool canWithdraw = riskEngine.canWithdraw(remainingCollateralUsd, debtUsd);
+
+        assertTrue(canWithdraw);
+    }
+
+    function testCanWithdrawReturnsFalse() public view {
+        uint256 remainingCollateralUsd = 70 ether;
+        uint256 debtUsd = 40 ether;
+
+        bool canWithdraw = riskEngine.canWithdraw(remainingCollateralUsd, debtUsd);
+
+        assertFalse(canWithdraw);
+    }
+
+    function testCanWithdrawAtExactLimit() public view {
+        uint256 remainingCollateralUsd = 100 ether;
+        uint256 debtUsd = 50 ether;
+
+        bool canWithdraw = riskEngine.canWithdraw(remainingCollateralUsd, debtUsd);
+
+        assertTrue(canWithdraw);
+    }
 
     //////////////////////////////////
     ////// LIQUIDATION TESTS ////////
