1a9e09b5ec96a8fdb667d2892b12c7c92fe3620a
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-06 06:17:58 +0000

    test: complete RiskEngine ownership coverage

--- test/unit/RiskEngine.t.sol
+++ test/unit/RiskEngine.t.sol
@@ -594,6 +594,61 @@ contract RiskEngineTest is RiskTestBase {
     ////// OWNER TESTS /////
     //////////////////////////
 
+    function testOwnerCanTransferOwnership() public {
+        address newOwner = makeAddr("New Owner");
+
+        riskEngine.transferOwnership(newOwner);
+
+        assertEq(riskEngine.owner(), newOwner);
+    }
+
+    function testNonOwnerCannotTransferOwnership() public {
+        address newOwner = makeAddr("New Owner");
+        address attacker = makeAddr("Attacker");
+
+        vm.prank(attacker);
+        vm.expectRevert(RiskEngine.RiskEngine__NotOwner.selector);
+
+        riskEngine.transferOwnership(newOwner);
+    }
+
+    function testNewOwnerCanCallOwnerFunctions() public {
+        address newOwner = makeAddr("New Owner");
+        uint256 newThreshold = 80;
+
+        riskEngine.transferOwnership(newOwner);
+
+        vm.prank(newOwner);
+
+        riskEngine.updateLiquidationThreshold(newThreshold);
+
+        assertEq(riskEngine.owner(), newOwner);
+        assertEq(riskEngine.liquidationThreshold(), newThreshold);
+    }
+
+    function testOldOwnerCannotCallOwnerFunctions() public {
+        address oldOwner = address(this);
+        address newOwner = makeAddr("New Owner");
+        uint256 newBonus = 15;
+
+        riskEngine.transferOwnership(newOwner);
+
+        vm.prank(oldOwner);
+        vm.expectRevert(RiskEngine.RiskEngine__NotOwner.selector);
+
+        riskEngine.updateLiquidationBonus(newBonus);
+
+        assertEq(riskEngine.owner(), newOwner);
+    }
+
+    function testTransferOwnershipToZeroAddressReverts() public {
+        address newOwner = address(0);
+
+        vm.expectRevert(RiskEngine.RiskEngine__InvalidOwner.selector);
+
+        riskEngine.transferOwnership(newOwner);
+    }
+
     /////////////////////////
     ///// PAUSE TESTS /////
     ////////////////////////
