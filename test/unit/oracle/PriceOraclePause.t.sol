5834b375ceb16ae73e5985f9f21f4c2b70e71709
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-07 08:19:13 +0000

    test: cover PriceOracle pause and access control

--- test/unit/oracle/PriceOraclePause.t.sol
+++ test/unit/oracle/PriceOraclePause.t.sol
@@ -2,15 +2,51 @@
 pragma solidity ^0.8.20;
 
 import {OracleTestBase} from "../../utils/OracleTestBase.t.sol";
+import {PriceOracle} from "../../../src/oracle/PriceOracle.sol";
 
 contract PriceOraclePauseTest is OracleTestBase {
-    function testOwnerCanPause() public {}
+    function testOwnerCanPause() public {
+        address owner = address(this);
 
-    function testOwnerCanUnpause() public {}
+        _pauseOracle();
 
-    function testNonOwnerCannotPause() public {}
+        assertEq(owner, oracle.owner());
+        assertTrue(oracle.paused());
+    }
 
-    function testNonOwnerCannotUnpause() public {}
+    function testOwnerCanUnpause() public {
+        address owner = address(this);
 
-    function testGetPriceRevertsWhenPaused() public {}
+        _pauseOracle();
+
+        assertEq(owner, oracle.owner());
+        assertTrue(oracle.paused());
+
+        _unpauseOracle();
+
+        assertFalse(oracle.paused());
+    }
+
+    function testNonOwnerCannotPause() public {
+        vm.prank(ATTACKER);
+        vm.expectRevert(PriceOracle.PriceOracle__NotOwner.selector);
+
+        oracle.unpause();
+    }
+
+    function testNonOwnerCannotUnpause() public {
+        _pauseOracle();
+
+        vm.prank(ATTACKER);
+        vm.expectRevert(PriceOracle.PriceOracle__NotOwner.selector);
+
+        oracle.unpause();
+    }
+
+    function testGetPriceRevertsWhenPaused() public {
+        _pauseOracle();
+
+        vm.expectRevert(PriceOracle.PriceOracle__Paused.selector);
+        oracle.getPrice();
+    }
 }
