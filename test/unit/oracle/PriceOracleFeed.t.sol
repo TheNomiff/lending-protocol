07d0b322c2cf859bdbf20d852e34506c820ac8fa
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-08 07:05:35 +0000

    test: cover core PriceOracle functionality and access control

--- test/unit/oracle/PriceOracleFeed.t.sol
+++ test/unit/oracle/PriceOracleFeed.t.sol
@@ -2,13 +2,41 @@
 pragma solidity ^0.8.20;
 
 import {OracleTestBase} from "../../utils/OracleTestBase.t.sol";
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+import {PriceOracle} from "../../../src/oracle/PriceOracle.sol";
 
 contract PriceOracleFeedTest is OracleTestBase {
     function testOwnerCanUpdateFeed() public {
-        
+        MockV3Aggregator newFeed = _deployMockFeed(3000e8);
+
+        _replaceFeed(address(newFeed));
+
+        assertEq(address(oracle.priceFeed()), address(newFeed));
     }
 
-    function testNonOwnerCannotUpdateFeed() public {}
+    function testNonOwnerCannotUpdateFeed() public {
+        MockV3Aggregator newFeed = _deployMockFeed(3000e8);
 
-    function testRevertsIfFeedZero() public {}
+        vm.prank(ATTACKER);
+
+        vm.expectRevert(PriceOracle.PriceOracle__NotOwner.selector);
+
+        oracle.setPriceFeed(address(newFeed));
+    }
+
+    function testRevertsIfFeedZero() public {
+        vm.expectRevert(PriceOracle.PriceOracle__InvalidFeed.selector);
+
+        oracle.setPriceFeed(address(0));
+    }
+
+    function testUpdatedFeedReturnsNewPrice() public {
+        MockV3Aggregator newFeed = _deployMockFeed(3000e8);
+
+        oracle.setPriceFeed(address(newFeed));
+
+        uint256 price = oracle.getPrice();
+
+        assertEq(price, 3000e8);
+    }
 }
