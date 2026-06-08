d005b3e6cfa381fa4c21f35eff34284516eaa819
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-08 06:27:32 +0000

    test: cover PriceOracle stale time validation scenarios

--- test/unit/oracle/PriceOracleStaleTime.t.sol
+++ test/unit/oracle/PriceOracleStaleTime.t.sol
@@ -2,9 +2,45 @@
 pragma solidity ^0.8.20;
 
 import {OracleTestBase} from "../../utils/OracleTestBase.t.sol";
+import {PriceOracle} from "../../../src/oracle/PriceOracle.sol";
 
 contract PriceOracleStaleTimeTest is OracleTestBase {
-    function testOwnerCanUpdateStaleTime() public {}
+    function testOwnerCanUpdateStaleTime() public {
+        uint256 newTime = 2 hours;
 
-    function testNonOwnerCannotUpdateStaleTime() public {}
+        _setStaleTime(newTime);
+
+        assertEq(oracle.staleTime(), newTime);
+    }
+
+    function testNonOwnerCannotUpdateStaleTime() public {
+        uint256 newTime = 2 hours;
+
+        vm.prank(ATTACKER);
+        vm.expectRevert(PriceOracle.PriceOracle__NotOwner.selector);
+
+        oracle.setStaleTime(newTime);
+    }
+
+    function testStaleTimeActuallyChangesValidationWindow() public {
+        uint256 newTime = 2 hours;
+
+        oracle.setStaleTime(newTime);
+
+        vm.warp(block.timestamp + 90 minutes);
+
+        uint256 price = oracle.getPrice();
+
+        assertEq(price, ETH_PRICE);
+    }
+
+    function testPriceRevertsAfterUpdatedStaleWindow() public {
+        oracle.setStaleTime(2 hours);
+
+        vm.warp(block.timestamp + 2 hours + 1);
+
+        vm.expectRevert(PriceOracle.PriceOracle__StalePrice.selector);
+
+        oracle.getPrice();
+    }
 }
