de7eeaf3c357c1c4a8c8e1e1f27950d833589e01
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-08 10:14:25 +0000

    test: complete PriceOracle ownership and access control coverage

--- test/unit/oracle/PriceOracleOwnership.t.sol
+++ test/unit/oracle/PriceOracleOwnership.t.sol
@@ -2,15 +2,61 @@
 pragma solidity ^0.8.20;
 
 import {OracleTestBase} from "../../utils/OracleTestBase.t.sol";
+import {PriceOracle} from "../../../src/oracle/PriceOracle.sol";
 
 contract PriceOracleOwnershipTest is OracleTestBase {
-    function testTransferOwnership() public {}
+    function testTransferOwnership() public {
+        address newOwner = makeAddr("New Onwer");
 
-    function testTransferOwnershipZeroAddressReverts() public {}
+        assertEq(oracle.owner(), address(this));
 
-    function testNonOwnerCannotTransferOwnership() public {}
+        _transferOwnership(newOwner);
 
-    function testNewOwnerCanCallOwnerFunctions() public {}
+        assertEq(oracle.owner(), newOwner);
+    }
 
-    function testOldOwnerCannotCallOwnerFunctions() public {}
+    function testTransferOwnershipZeroAddressReverts() public {
+        address newOwner = address(0);
+
+        vm.expectRevert(PriceOracle.PriceOracle__InvalidOwner.selector);
+
+        _transferOwnership(newOwner);
+    }
+
+    function testNonOwnerCannotTransferOwnership() public {
+        address newOwner = makeAddr("New Onwer");
+
+        vm.prank(ATTACKER);
+        vm.expectRevert(PriceOracle.PriceOracle__NotOwner.selector);
+
+        oracle.transferOwnership(newOwner);
+    }
+
+    function testNewOwnerCanCallOwnerFunctions() public {
+        uint256 newStaleTime = 2 hours;
+        address newOwner = makeAddr("New Onwer");
+
+        _transferOwnership(newOwner);
+
+        vm.prank(newOwner);
+
+        oracle.setStaleTime(newStaleTime);
+
+        assertEq(oracle.staleTime(), newStaleTime);
+    }
+
+    function testOldOwnerCannotCallOwnerFunctions() public {
+        uint256 newStaleTime = 2 hours;
+        address newOwner = makeAddr("New Onwer");
+        address oldOwner = address(this);
+
+        _transferOwnership(newOwner);
+
+        assertEq(oracle.owner(), newOwner);
+
+        vm.prank(oldOwner);
+        vm.expectRevert(PriceOracle.PriceOracle__NotOwner.selector);
+
+        oracle.setStaleTime(newStaleTime);
+    }
 }
