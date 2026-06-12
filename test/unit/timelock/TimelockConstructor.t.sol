c176a063264d13fe02e884de5c808a321afcefd4
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-12 18:44:41 +0000

    test: cover timelock constructor initialization

--- test/unit/timelock/TimelockConstructor.t.sol
+++ test/unit/timelock/TimelockConstructor.t.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.20;
 import {TimelockTestBase} from "../../utils/TimelockTestBase.t.sol";
 
 contract TimelockConstructorTest is TimelockTestBase {
-    function testConstructorSetsOwner() public {
-        // Verifies that constructor assigns owner to msg.sender (deployer).
+    function testConstructorSetsOwner() public view {
+        assertEq(timelock.owner(), address(this));
     }
 
-    function testConstructorSetsTxCountToZero() public {
-        // Verifies that txCount is initialized to zero before any transaction is queued.
+    function testConstructorSetsTxCountToZero() public view {
+        assertEq(timelock.txCount(), 0);
     }
 
-    function testConstructorSetsDelayConstant() public {
-        // Verifies that DELAY is set to the expected fixed value (1 days).
+    function testConstructorSetsDelayConstant() public view {
+        assertEq(timelock.DELAY(), 1 days);
     }
 }
