1487dd71295c217be6f1fcb95798265a711bc832
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-16 17:38:11 +0000

    test: separate liquidation and risk test infrastructure

--- test/utils/LiquidationTestBase.t.sol
+++ test/utils/LiquidationTestBase.t.sol
@@ -2,19 +2,20 @@
 pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
+import {LiquidationEngine} from "../../src/engines/LiquidationEngine.sol";
 import {RiskEngine} from "../../src/engines/RiskEngine.sol";
 
-abstract contract RiskTestBase is Test {
+abstract contract LiquidationTestBase is Test {
+    LiquidationEngine internal liquidationEngine;
     RiskEngine internal riskEngine;
 
     address internal owner = address(this);
-    address internal constant GUARDIAN = address(100);
-    address internal constant USER = address(1);
 
     uint256 internal constant SUPPLY_CAP = 1000 ether;
     uint256 internal constant BORROW_CAP = 500 ether;
 
     function setUp() public virtual {
         riskEngine = new RiskEngine(SUPPLY_CAP, BORROW_CAP);
+        liquidationEngine = new LiquidationEngine(address(riskEngine));
     }
 }
