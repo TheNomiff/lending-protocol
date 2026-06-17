11e86506c0913a3bdd77701cc95ac16b2b443752
TheNomiff <208472182+TheNomiff@users.noreply.github.com>
2026-06-17 09:31:20 +0000

    test: cover LiquidationEngine constructor initialization

--- test/unit/liquidation/LiquidationEngineConstructor.t.sol
+++ test/unit/liquidation/LiquidationEngineConstructor.t.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.20;
 
 import {LiquidationTestBase} from "../../utils/LiquidationTestBase.t.sol";
+import {LiquidationEngine} from "../../../src/engines/LiquidationEngine.sol";
 
 contract LiquidationEngineConstructorTest is LiquidationTestBase {
     function testConstructorSetsOwner() public view {
         assertEq(liquidationEngine.owner(), address(this));
+    }
+
+    function testConstructorRevertsIfRiskEngineZeroAddress() public {
+        vm.expectRevert(
+            LiquidationEngine.LiquidationEngine__InvalidRiskEngine.selector
+        );
+
+        new LiquidationEngine(address(0));
     }
 }
